6565e983815cdfc519165155105fb3f1122e6563
Cindy <759501948@qq.com>
2017-03-04 15:36:04 +0800

    display weather2.

--- app/src/main/java/com/example/admin/weather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/example/admin/weather/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.example.admin.weather.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -67,6 +70,13 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPreferences.getBoolean("city_selected",false)){
+            Intent intent = new Intent(this,WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -84,6 +94,11 @@ public class ChooseAreaActivity extends Activity {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(index);
                     queryCountries();
+                }else if (currentLevel == LEVEL_COUNTRY){
+                    String countryCode = countryList.get(index).getCountryCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
